e915ae9af2a2c41619bd2bf42be86184286641f5
VictorZhanUnity <73773024+VictorZhanUnity@users.noreply.github.com>
2024-05-20 19:11:22 +0800

    Update StringHandler.cs

--- UnityProject/WakeFun/Assets/_VictorDEV/Common/StringHandler.cs
+++ UnityProject/WakeFun/Assets/_VictorDEV/Common/StringHandler.cs
@@ -1,3 +1,7 @@
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
 namespace VictorDev.Common
 {
     public abstract class StringHandler
@@ -6,5 +10,16 @@ namespace VictorDev.Common
         /// 設置文字大小(HTML)
         /// </summary>
         public static string SetFontSizeString(string str, int fontSize) => $"<size='{fontSize}'>{str}</size>";
+
+        /// <summary>
+        /// 解碼Base64 byte[] 轉成UTF8字串
+        /// </summary>
+        public static string Base64ToString(byte[] data)
+        {
+            string base64String = JsonConvert.SerializeObject(data).Trim('\"');
+            byte[] byteArray = Convert.FromBase64String(base64String);
+            // 將 byte[] 解碼為字符串
+            return Encoding.UTF8.GetString(byteArray);
+        }
     }
 }
